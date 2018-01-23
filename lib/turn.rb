1ac85a7dce365495f8deac6ab53d7aa20126c6a3
Elle Wilson <eleanorwilsonn@gmail.com>
2018-01-23 17:02:09 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,40 @@
+def turn(board)
+  puts "Please enter 1-9:"
+end
+
+
+def display_board(board)
+  puts "   " "|" "   " "|" "   "
+  puts "-----------"
+  puts "   " "|" "   " "|" "   "
+  puts "-----------"
+  puts "   " "|" "   " "|" "   "
+end
+
+def input_to_index
+  input = gets
+  input.to_i = position
+end
+
+def valid_move?
+  if board[position].to_i == between?(1,9)
+    true
+  if position_taken? == "false"
+    true
+  else
+    false
+  end
+end
+
+def position_taken?(board,position)
+  if board[position] == " "
+    false
+    elsif board[position] == ""
+    false
+    elsif  board[position] == nil
+    false
+    else   board[position] == "X" || "O"
+    true
+  end
+end
+end
